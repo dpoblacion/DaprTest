f7a9f6348c10109353f9dd4375e230b4d273b44e
David <dpoblacion@globalavl.com>
2021-04-02 10:48:57 +0200

    Add project files.

---
+++ src/Publisher/Controllers/ValueController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Publisher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly DaprClient _client;
+
+        private readonly ILogger<ValueController> _logger;
+
+        public ValueController(DaprClient client, ILogger<ValueController> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(string text)
+        {
+            await _client.PublishEventAsync<TextMessage>("pubsub", "newOrder", new TextMessage()
+            {
+                Text = text
+            });
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Subscriber2/Controllers/ValueController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Messages;
+using Dapr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Subscriber2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly ILogger<ValueController> _logger;
+
+        public ValueController(ILogger<ValueController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Topic("pubsub", "newOrder")]
+        [HttpPost]
+        public async Task<ActionResult> CreateMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+
+            await Task.Delay(1);
+
+            return Ok();
+        }
+    }
+}
